48cca38bdcd185e9c05baada3729c4fc2ca8a8bd
Brian Hannafious <32105697+xbrianh@users.noreply.github.com>
2026-09-03 11:43:47 -0700

    wip

--- crates/gremlins/src/config.rs
+++ crates/gremlins/src/config.rs
@@ -260,7 +260,7 @@ pub fn resolve_state_root(overrides: Option<&PathOverrides>) -> PathBuf {
         }
     }
     let p = dirs::state_dir()
-        .or_else(|| dirs::data_dir())
+        .or_else(dirs::data_dir)
         .unwrap_or_else(|| PathBuf::from("."))
         .join("gremlins");
     std::fs::create_dir_all(&p).ok();
